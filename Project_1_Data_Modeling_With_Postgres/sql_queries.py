d3279c8ca3650a4f4b114446058698143e37c839
juan-ivan-NV <coldcram14@hotmail.com>
2021-03-06 22:21:18 -0600

    inserting data queries completed

--- Project_1_Data_Modeling_With_Postgres/sql_queries.py
+++ Project_1_Data_Modeling_With_Postgres/sql_queries.py
@@ -20,19 +20,20 @@ time_table_create = ("CREATE TABLE IF NOT EXISTS time (start_time time, hour int
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+songplay_table_insert = (""" INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""")
+
+user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
 """)
 
-user_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
 """)
 
-song_table_insert = ("INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)")
-
-artist_table_insert = ("""
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
 """)
 
 
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
