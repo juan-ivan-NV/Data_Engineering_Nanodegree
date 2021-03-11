d40baeaff15a506ff057c21a494310b7bd7fd614
juan-ivan-NV <coldcram14@hotmail.com>
2021-03-10 23:28:43 -0600

    structure COPY command

--- Project_1_Data_Modeling_With_Postgres/sql_queries.py
+++ Project_1_Data_Modeling_With_Postgres/sql_queries.py
@@ -8,11 +8,11 @@ time_table_drop = "DROP TABLE time"
 
 # CREATE TABLES
 
-songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays (songplay_id int, start_time time, user_id int, level varchar, song_id int, artist_id int, session_id int, location varchar, user_agent varchar);")
+songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY NOT NULL, start_time time, user_id INT NOT NULL, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);")
 
 user_table_create = ("CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar, last_name varchar, gender varchar, level varchar);")
 
-song_table_create = ("CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, year int, duration float);")
+song_table_create = ("CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, year int, duration numeric);")
 
 artist_table_create = ("CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar, location varchar, latitude numeric, longitude numeric);")
 
@@ -20,13 +20,19 @@ time_table_create = ("CREATE TABLE IF NOT EXISTS time (start_time time, hour int
 
 # INSERT RECORDS
 
-songplay_table_insert = (""" INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""")
+songplay_table_insert = (""" INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""")
 
 user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
+""")
+
+song_table_insert_cp = (""" create function pg_ls_dir2(text) as 'SELECT pg_ls_dir($1)'
+                            FOR filename IN SELECT pg_ls_dir2('data/log_data/2018/11') LOOP
+                                IF (filename ~ '.json$') THEN
+                                    COPY 'data/log_data/2018/11' || TO 
 """)
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
@@ -38,7 +44,8 @@ time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, ye
 
 # FIND SONGS
 
-song_select = ("""
+song_select = (""" SELECT s.song_id, s.artist_id FROM songs s INNER JOIN artists a ON s.artist_id = a.artist_id
+                    WHERE s.title = %s AND a.name = %s AND s.duration = %s;
 """)
 
 # QUERY LISTS
