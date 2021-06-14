0a6a2dbe68e9f99605d6eac1ec58d0de8bca37a3
juan-ivan-NV <coldcram14@hotmail.com>
2021-06-13 21:32:29 -0500

    working on sql queries

--- 16_Project_5_Capstone_Project/README.md
+++ 16_Project_5_Capstone_Project/README.md
@@ -1,20 +1,44 @@
 # Data engineering capstone project
 
-Data:
+## Data sources.
 
 * <a href= "https://public.opendatasoft.com/explore/dataset/us-cities-demographics/export/">US-cities-demographics.csv</a> / Demograpjic data for cities
 
-Columns ► city - state - media_age - male_population - female_population - total_population - num_veterans - foreign_born - average_household_size - state_code - race - count
+<pre>Columns ► city - state - media_age - male_population - female_population - total_population - num_veterans - foreign_born - average_household_size - state_code - race - count</pre>
 
 * <a href= "https://www.trade.gov/national-travel-and-tourism-office">immigration_data_sample.csv</a> / l94 imigration data
 
-Columns ► cicid - year - month - cit - res - iata - arrdate - mode - addr - depdate - bir - visa - coun- dtadfil - visapost - occup - entdepa - entdepd - entdepu - matflag - biryear - dtaddto - gender - insnum - airline - admnum - fltno - visatype
+<pre>Columns ► cicid - year - month - cit - res - iata - arrdate - mode - addr - depdate - bir - visa - coun- dtadfil - visapost - occup - entdepa - entdepd - entdepu - matflag - biryear - dtaddto - gender - insnum - airline - admnum - fltno - visatype</pre>
 
 * <a href= "https://datahub.io/core/airport-codes#data">airport-codes_csv.csv</a> / information related to airports
 
-Columns ► ident - type - name - elevation_ft - continent - iso_country - iso_region - municipality - gps_code - iata_code - local_code - coordinates
+<pre>Columns ► ident - type - name - elevation_ft - continent - iso_country - iso_region - municipality - gps_code - iata_code - local_code - coordinates</pre>
 
 * <a href= "https://www.kaggle.com/berkeleyearth/climate-change-earth-surface-temperature-data">GlobalLandTemperaturesByCity.csv</a> / Kaggle source (This study combines 1.6 billion temperature reports from 16 pre-existing archives)
 
-Columns ► dt - AverageTemperature - AverageTemperatureUncertainity - City - Country - Latitude - Longitude.
+<pre>Columns ► dt - AverageTemperature - AverageTemperatureUncertainity - City - Country - Latitude - Longitude</pre>
+
+
+## Data exploration.
+
+
+
+## Data selection and cleaning.
+
+
+
+## Database creation to redshift.
+
+
+
+### Schema.
+
+
+
+## Consuming data (some analitics queries).
+
+
+
+
+
 
---
+++ 16_Project_5_Capstone_Project/sql_queries.py
@@ -0,0 +1,77 @@
+# Drop tables
+
+drop_airports = "DROP TABLE IF EXISTS airports;"
+drop_demographics = "DROP TABLE IF EXISTS demographics;"
+drop_immigrations = "DROP TABLE IF EXISTS immigrations;"
+drop_temperatures = "DROP TABLE IF EXISTS temperatures;"
+
+# Create tables
+
+create_airports = """CREATE TABLE IF NOT EXISTS airports(
+                        iata_code              VARCHAR PRIMARY KEY,
+                        name                   VARCHAR,
+                        type                   VARCHAR,
+                        local_code             VARCHAR,
+                        coordinates            VARCHAR,
+                        city                   VARCHAR,
+                        elevation_ft           VARCHAR,
+                        continent              VARCHAR,
+                        iso_country            VARCAHR,
+                        iso_region             VARCHAR,
+                        municipality           VARCHAR,
+                        gps_code               VARCHAR
+                        );"""
+
+create_demographics = """CREATE TABLE IF NOT EXISTS demographics(
+                        city                   VARCAHR,
+                        state                  VARCHAR,
+                        media_age              FLOAT,
+                        male_population        INT,
+                        female_populaiton      INT,
+                        total_population       INT,
+                        num_veterans           INT,
+                        foreign_born           INT,
+                        average_household_size FLOAT,
+                        state_code             VARCHAR(2),
+                        race                   VARCHAR,
+                        count                  INT
+);"""
+
+create_immigrations = """CREATE TABLE IF NOT EXISTS immigrations(
+                        cicid                  FLOAT PRIMARY KEY,
+                        year                   FLOAT,
+                        month                  FLOAT,
+                        cit                    FLOAT,
+                        res                    FLOAT,
+                        iata                   VARCHAR(3),
+                        arrdate                FLOAT,
+                        mode                   FLOAT,
+                        addr                   VARCHAR,
+                        depdate                FLOAT,
+                        bir                    FLOAT,
+                        visa                   FLOAT,
+                        count                  FLOAT,
+                        dtadfile               VARCHAR,
+                        entdepa                VARCHAR(1),
+                        entdepd                VARCHAR(1),
+                        matflag                VARCHAR(1),
+                        biryear                FLOAT,
+                        dtaddto                VARCHAR,
+                        gender                 VARCHAR(1),
+                        airline                VARCHAR,
+                        admnum                 FLOAT,
+                        fltno                  VARCHAR,
+                        visatype               VARCHAR
+);"""
+
+create_temperatures = """CREATE TABLE IF NOT EXISTS temperatures(
+                        timestamp                      DATE,
+                        average_temperature            FLOAT,
+                        average_temperature_uncertainty FLOAT,
+                        city                           VARCHAR,
+                        country                        VARCHAR,
+                        latitude                       VARCHAR,
+                        longitude                      VARCHAR
+);"""
+
+# Insert data
