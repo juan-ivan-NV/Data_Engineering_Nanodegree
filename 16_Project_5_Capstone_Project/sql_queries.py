7ccd48d42346ba8f5ae53947d09f9dadfce54ace
juan-ivan-NV <coldcram14@hotmail.com>
2021-06-29 23:50:54 -0500

    queries ready nex step testings

--- 16_Project_5_Capstone_Project/sql_queries.py
+++ 16_Project_5_Capstone_Project/sql_queries.py
@@ -7,75 +7,65 @@ drop_temperatures = "DROP TABLE IF EXISTS temperatures;"
 
 # Create tables
 
+create_immigrations = """CREATE TABLE IF NOT EXISTS immigrations(
+                        cicid                   FLOAT PRIMARY KEY,
+                        i94yr                   FLOAT,
+                        i94mon                  FLOAT,
+                        i94cit                  FLOAT,
+                        i94res                  FLOAT,
+                        i94port                 CHAR(3),
+                        arrdate                 FLOAT,
+                        i94mode                 FLOAT,
+                        i94addr                 VARCHAR,
+                        depdate                 FLOAT,
+                        i94bir                  FLOAT,
+                        i94visa                 FLOAT,
+                        dtadfile                VARCHAR,
+                        matflag                 CHAR(1),
+                        biryear                 FLOAT,
+                        dtaddto                 VARCHAR,
+                        gender                  CHAR(1),
+                        airline                 VARCHAR,
+                        admnum                  FLOAT,
+                        fltno                   VARCHAR,
+                        visatype                VARCHAR
+);"""
+
+
 create_airports = """CREATE TABLE IF NOT EXISTS airports(
-                        iata_code              VARCHAR PRIMARY KEY,
-                        name                   VARCHAR,
-                        type                   VARCHAR,
-                        local_code             VARCHAR,
-                        coordinates            VARCHAR,
-                        city                   VARCHAR,
-                        elevation_ft           VARCHAR,
-                        continent              VARCHAR,
-                        iso_country            VARCAHR,
-                        iso_region             VARCHAR,
-                        municipality           VARCHAR,
-                        gps_code               VARCHAR
+                        ident                   VARCHAR,
+                        type                    VARCHAR,
+                        name                    VARCHAR,
+                        elevation_ft            FLOAT,
+                        continent               VARCHAR,
+                        iso_region              VARCHAR,
+                        municipality            VARCHAR,
+                        coordinates             VARCHAR
                         );"""
 
 create_demographics = """CREATE TABLE IF NOT EXISTS demographics(
-                        city                   VARCAHR,
-                        state                  VARCHAR,
-                        media_age              FLOAT,
-                        male_population        INT,
-                        female_populaiton      INT,
-                        total_population       INT,
-                        num_veterans           INT,
-                        foreign_born           INT,
-                        average_household_size FLOAT,
-                        state_code             VARCHAR(2),
-                        race                   VARCHAR,
-                        count                  INT
-);"""
-
-create_immigrations = """CREATE TABLE IF NOT EXISTS immigrations(
-                        cicid                  FLOAT PRIMARY KEY,
-                        year                   FLOAT,
-                        month                  FLOAT,
-                        cit                    FLOAT,
-                        res                    FLOAT,
-                        iata                   VARCHAR(3),
-                        arrdate                FLOAT,
-                        mode                   FLOAT,
-                        addr                   VARCHAR,
-                        depdate                FLOAT,
-                        bir                    FLOAT,
-                        visa                   FLOAT,
-                        count                  FLOAT,
-                        dtadfile               VARCHAR,
-                        entdepa                VARCHAR(1),
-                        entdepd                VARCHAR(1),
-                        matflag                VARCHAR(1),
-                        biryear                FLOAT,
-                        dtaddto                VARCHAR,
-                        gender                 VARCHAR(1),
-                        airline                VARCHAR,
-                        admnum                 FLOAT,
-                        fltno                  VARCHAR,
-                        visatype               VARCHAR
+                        city                    VARCAHR,
+                        state                   VARCHAR,
+                        median_age              FLOAT,
+                        male_population         INT,
+                        female_population       INT,
+                        total_population        INT,
+                        number_of_veterans      INT,
+                        foreign_born            INT,
+                        average_household_size  FLOAT,
+                        state_code              CHAR(2),
+                        Race                    VARCHAR,
+                        Count                   INT
 );"""
 
 create_temperatures = """CREATE TABLE IF NOT EXISTS temperatures(
-                        timestamp                       DATE,
-                        average_temperature             FLOAT,
-                        average_temperature_uncertainty FLOAT,
-                        city                            VARCHAR,
-                        country                         VARCHAR,
-                        latitude                        VARCHAR,
-                        longitude                       VARCHAR
+                        dt                             DATE,
+                        AverageTemperature             FLOAT,
+                        AverageTemperatureUncertainty  FLOAT,
+                        City                           VARCHAR,
+                        Country                        VARCHAR,
+                        Latitude                       VARCHAR,
+                        Longitude                      VARCHAR
 );"""
 
 # Insert data
-
-drop_table_queries = [drop_airports, drop_demographics, drop_immigrations, drop_temperatures]
-create_table_queries = [create_airports, create_demographics, create_immigrations, create_temperatures]
-
