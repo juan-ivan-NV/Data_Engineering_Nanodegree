24eb2afa7ace650c0769c1035dffd615a6fff74c
juan-ivan-NV <coldcram14@hotmail.com>
2021-07-12 21:52:35 -0500

    csv to s3 all functions finished

--- 16_Project_5_Capstone_Project/get_csv_to_s3.py
+++ 16_Project_5_Capstone_Project/get_csv_to_s3.py
@@ -29,9 +29,54 @@ def drop_cols(df, *columns_to_drop):
     
     return df
 
+
 # Function to drop rows with nulls
+def delete_nulls(df, *columsn_w_nulls):
+    return df.na.drop(subset=*columsn_w_nulls)
 
+    
+# SAS_values_tables
+def sas_value_parser(value, columns):
+    """Parses SAS Program file to return value as pandas dataframe
+    Args:
+        value (str): sas value to extract.
+        columns (list): list of 2 containing column names.
+    Return:
+        None
+    """
+    file = 'I94_SAS_Labels_Descriptions.SAS'
+    
+    file_string = ''
+    
+    with open(file) as f:
+        file_string = f.read()
+    
+    file_string = file_string[file_string.index(value):]
+    file_string = file_string[:file_string.index(';')]
+    
+    line_list = file_string.split('\n')[1:]
+    codes = []
+    values = []
+    
+    for line in line_list:
+        
+        if '=' in line:
+            code, val = line.split('=')
+            code = code.strip()
+            val = val.strip()
 
+            if code[0] == "'":
+                code = code[1:-1]
+
+            if val[0] == "'":
+                val = val[1:-1]
+
+            codes.append(code)
+            values.append(val)
+        
+            
+    return pd.DataFrame(list(zip(codes, values)), columns=columns)
+    
 
 # Function to upload csv files to S3
 
@@ -50,17 +95,42 @@ def csv_s3(df, file_name, s3_path):
         s3_resource.Object(bucket, file_name).put(Body=csv_buffer.getvalue())
 
     print("{} successfully submitted to {}".format(file_name, s3_path))
+
+    
 def main():
     
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
-    path = *config['BUCKET'].values()
+    path = "s3://capstoneprojectde"
     
-    # immigrations df
+    print("--------Working on immigrations file--------►")
     immigrations_df_spark = spark.read.load('./sas_data')
     immigrations_df_spark = drop_cols(immigrations_df_spark, ['visapost', 'occup', 'entdepu', 'insnum', 'entdepa', 'entdepd', 'entdepd', 'count', 'adnum'])
-    # 'immigrations_data.csv'
+    csv_s3(immigrations_df_spark, 'immigrations_data.csv', path)
+    
+    print("--------Working on i94_residence file--------►")
+    i94_residence = sas_value_parser('i94cntyl', ['i94cit_res', 'country'])
+    csv_s3(i94_residence, 'i94_residence.csv', path)
+    
+    print("--------Working on i94_port_of_admission file--------►")
+    i94_port_of_admission = sas_value_parser('i94prtl', ['i94port', 'port'])
+    csv_s3(i94_port_of_admission, 'i94_port_of_admission.csv', path)
+    
+    print("--------Working on i94_usa_state_arrival file--------►")
+    i94_usa_state_arrival = sas_value_parser('i94addrl', ['i94addr', 'state'])
+    csv_s3(i94_usa_state_arrival, 'i94_usa_state_arrival.csv', path)
+    
+    print("--------Working on demographics_data file--------►")
+    demographics_sp_df = spark.read.option("delimiter",";").option("header", True).csv('us-cities-demographics.csv')
+    csv_s3(demographics_sp_df, 'demographics_data.csv', path)
+    
+    print("--------Working on Temperature_data file--------►")
+    temp_sp_df = spark.read.option("header", True).csv('../../data2/GlobalLandTemperaturesByCity.csv')
+    temp_sp_df = delete_nulls(temp_sp_df, ["AverageTemperature","AverageTemperatureUncertainty"])
+    csv_s3(temp_sp_df, 'Temperature_data.csv', path)
+    
     # 
-
-
-
+    
+    # SAS tables
+    
+    
